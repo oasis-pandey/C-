27af149d6b1aa08253829eb893e5465f3dd35aa6
oasis-pandey <oasispandey2000@gmail.com>
2024-03-11 13:25:06 +0545

    getline function

--- user_input.cpp
+++ user_input.cpp
@@ -5,10 +5,18 @@ int main(){
     std::string name;
     std::cout << "What is your age?";
     std::cin >> age;
+ 
+    std::string brother;
+    std::cout<<"What is the name of your brother?";
+    std::getline(std::cin>>std::ws,brother);
 
     std::cout << "What is your name ?";
-    std::cin>>name;
+    std::getline(std::cin>> std::ws,name);//used to get input strings for more than 1 word
+
     std:: cout << "Your name is "<<name<<"\n";
     std::cout << "You are "<<age<<" years old"<<"\n";
+
+    std:: cout <<"Your brother's name is "<< brother<<"\n";
+
     return 0;
 }
