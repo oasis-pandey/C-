92b2e951f8f152c30e0ef66732dea5861ff59ad3
oasis-pandey <oasispandey2000@gmail.com>
2024-03-11 13:48:51 +0545

    math functions

---
+++ math_functions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+int main(){
+    int x=2;
+    int y=3;
+    int z;
+    // z= std::max(x,y);
+    // z= std::min(x,y);
+    // z=abs(-5);
+    // z= pow(x,y);
+    // z= round (3.14);
+    // z=sqrt(9);
+    // z=ceil(3.14); rounds off to greater
+    // z=floor(3.99999); round off to lower
+    std::cout<<z;
+
+    return 0;
+}
